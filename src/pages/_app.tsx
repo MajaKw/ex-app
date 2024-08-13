81051c4d44ce7b13d825ba8ed8a6bc91b12c6918
maisho <13maja.kwiatek@gmail.com>
2024-08-13 21:50:11 +0200

    connecting Clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import type { AppType } from 'next/app';
 import { trpc } from '../utils/trpc';
+
+// import '@/styles/globals.css'
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import type { AppProps } from 'next/app'
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 };
 export default trpc.withTRPC(MyApp);
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { trpc } from '../utils/trpc';
 
 export default function IndexPage() {
+  // const hello = trpc.getCountry.useQuery();
+  // console.log(hello.data)
+  // if (!hello.data) {
+  //   return <div>Loading...</div>;
+  // }
   return (
     <div >
-      Hey
+      Hello
+      {/* {
+        hello.data.map((el)=><p key={el.name}>{el.name}</p>)
+      } */}
     </div>
   );
 }
