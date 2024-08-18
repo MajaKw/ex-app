701d298ee8c2fba2714d5784a3b4aad7134fa79d
maisho <13maja.kwiatek@gmail.com>
2024-08-18 04:10:02 +0200

    save

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,46 +1,40 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server';
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripeClient = require('stripe')(process.env.STRIPE_SECRET_KEY);
+export async function POST(req: Request) {
+  try {
+    console.log("try 1")
+    console.log
+    const session = await stripeClient.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "pln",
+            product_data: {
+              name: "apka do korków pro",
+              description: "LEGENDARNA",
+            },
+            unit_amount: 5000,
+            // recurring: {
+            //   interval: 'month',
+            // },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${req.headers.get('origin')}/?success=true`,
+      cancel_url: `${req.headers.get('origin')}/?canceled=true`,
+    });
 
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-            
-            price_data: {
-                currency: "pln",
-                product_data: {
-                    name: "apka do korków pro",
-                    description: "LEGENDARNA"
-                },
-                unit_amount: 5000,
-                recurring: {
-                    interval: 'month'
-                }
-            },
-              quantity: 1,
-            },
-          ],
-        mode: 'subscription',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
-      res.redirect(303, session.url);
-    } catch (error) {
-      reportError({ message: getErrorMessage(error) })
-      // res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    console.log("try 2")
+    console.log(session.url)
+    // Redirect to the Stripe Checkout session URL
+    return NextResponse.redirect(session.url);
+  } catch (error) {
+    const errorMessage = (error instanceof Error) ? error.message : 'Internal Server Error';
+
+    // Return an error response with status 500
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
-function getErrorMessage(error: unknown) {
-	if (error instanceof Error) return error.message
-	return String(error)
-}
-
-export { handler as POST}
